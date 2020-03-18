9fbac37cbff3bdb9cd63fdc1f0b0fc03e8541ee6
Nicholas Eskov <mr.eskov1@yandex.ru>
2020-03-19 00:37:44 +0300

    Initial commit

---
+++ scripts/run_static_checks.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+static_check() {
+  local olddir
+  olddir="$(pwd)"
+
+  local projdir="$1"
+  cd "$projdir" || exit 1
+
+  mkdir -p "build" && cd "build" || exit 1
+  cmake .. -DENABLE_CPPCHECK=ON || exit 1
+  cd ..
+
+  local projname
+  if [ "$2" != "" ]; then
+    projname=$2
+  else
+    projname="$(pwd | rev | cut -d "/" -f 1 | rev)"
+  fi
+
+  cd "build" || exit 1
+  make "$projname"_check || return $?
+
+  cd "$olddir" || exit 1
+}
+
+static_check "$2" "hw"
+
+exitstaus=$?
+
+libsdir=$1
+if [ -n "$libsdir" ]; then
+  cd "$libsdir" || exit 1
+
+  libs="$(echo ./*/ || exit 1)"
+
+  if [ "$libs" != "./*/" ]; then
+    for d in $libs; do
+      static_check "$d"
+      exitstaus=$((exitstaus + $?))
+    done
+  fi
+fi
+
+exit $exitstaus
