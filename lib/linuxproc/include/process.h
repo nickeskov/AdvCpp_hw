0b900113719d276e3bde266bac82038ef42b28ab
Nicholas Eskov <mr.eskov1@yandex.ru>
2020-03-20 06:42:38 +0300

    understanding and upgrading example from 'man pipe'

---
+++ lib/linuxproc/include/process.h
@@ -0,0 +1,29 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include <string>
+#include <cstddef>
+
+class Process {
+  public:
+
+    explicit Process(const std::string &path);
+
+    size_t write(const void *data, size_t len);
+
+    void writeExact(const void *data, size_t len);
+
+    size_t read(void *data, size_t len);
+
+    void readExact(void *data, size_t len);
+
+    bool isReadable() const;
+
+    void closeStdin();
+
+    ~Process() noexcept;
+
+  private:
+};
+
+#endif //PROCESS_H
